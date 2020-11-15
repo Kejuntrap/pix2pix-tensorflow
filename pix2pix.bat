fabf97a76bb9e962335a93598d530f9f92dc49c0
Jhori <43299846+Kejuntrap@users.noreply.github.com>
2020-11-16 01:31:44 +0900

    Update pix2pix.bat

--- pix2pix.bat
+++ pix2pix.bat
@@ -1,4 +1,3 @@
-cd pix2pix-tensorflow
 python pix2pix.py --mode train --output_dir facades_train --max_epochs 100 --input_dir facades\train  --which_direction BtoA
 python pix2pix.py   --mode test   --output_dir facades_test   --input_dir facades/val   --checkpoint facades_train
 pause
